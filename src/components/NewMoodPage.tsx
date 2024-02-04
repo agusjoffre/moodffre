39d1d5de508e6ad6cd3494afc8f5d1ba9ca8d7c2
agusjoffre <aguslj16@gmail.com>
2024-02-04 15:11:15 -0300

    feat: carousel for mood input

--- src/components/NewMoodPage.tsx
+++ src/components/NewMoodPage.tsx
@@ -5,20 +5,37 @@ import MoodForm from './MoodForm'
 import ActivityForm from './ActivityForm'
 import DescriptionForm from './DescriptionForm'
 import MoodDetails from './MoodDetails'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from '@/components/ui/carousel'
 
 export default function NewMoodPage (): JSX.Element {
   /* mood es un objeto de tipo Mood */
   const [mood, setMood] = useState<Mood | null>(null)
   return (
-        <main>
-            { /*
-            primero mood y despues activity, despues description,
-            despues detalles del mood completo y despues submit
-            */}
-      <MoodForm />
-      <ActivityForm />
-      <DescriptionForm />
-      <MoodDetails/>
+        <main className='min-h-screen min-w-screen flex flex-col gap-10 items-center'>
+      <Carousel className='w-1/2 gap-1'>
+        <CarouselContent >
+            <CarouselItem>
+              <MoodForm />
+            </CarouselItem>
+            <CarouselItem>
+              <ActivityForm />
+          </CarouselItem>
+            <CarouselItem>
+              <DescriptionForm />
+          </CarouselItem>
+          <CarouselItem>
+              <MoodDetails/>
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious size='icon'/>
+        <CarouselNext size='icon'/>
+      </Carousel>
         </main>
   )
 }
